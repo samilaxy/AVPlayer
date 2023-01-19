54cd5da461e1da571fc39e4ff54bd2b2bcc0f942
Noye Samuel <59480282+samilaxy@users.noreply.github.com>
2023-01-19 08:14:12 +0000

    Update ViewController.swift

--- ViewController.swift
+++ ViewController.swift
@@ -20,33 +20,33 @@ class ViewController: UIViewController {
 // https://aegis-dev.s3.amazonaws.com/summarized_videos/69946f1d-9e73-402e-9947-236be4e9dd16/1726/highlights/highlights.mp4?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVFNIZGJGSZF3D6CY/20230118/eu-central-1/s3/aws4_request&X-Amz-Date=20230118T091446Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=154e70dbeb81951fdb55bf79c1c289fb65b9c5595f253df1b1fafc445eaedb3d
     @available(iOS 16.0, *)
     @IBAction func btnPlay(_ sender: Any) {
-        guard let url = URL(string: "https://streamable.com/0ntwbq") else { return }
+//        guard let url = URL(string: "https://aegis-dev.s3.amazonaws.com/summarized_videos/69946f1d-9e73-402e-9947-236be4e9dd16/1726/highlights/highlights.mp4?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAVFNIZGJGSZF3D6CY/20230118/eu-central-1/s3/aws4_request&X-Amz-Date=20230118T091446Z&X-Amz-Expires=86400&X-Amz-SignedHeaders=host&X-Amz-Signature=154e70dbeb81951fdb55bf79c1c289fb65b9c5595f253df1b1fafc445eaedb3d") else { return }
         
-//        if let path = Bundle.main.path(forResource: "highlights", ofType: "mp4"){
-//            let video = AVPlayer(url: URL(filePath: path))
+        if let path = Bundle.main.path(forResource: "highlights", ofType: "mp4"){
+            let video = AVPlayer(url: URL(filePath: path))
+            
+            let controller = AVPlayerViewController()
+            controller.player = video
+            
+                // Modally present the player and call the player's play() method when complete.
+            present(controller, animated: true) {
+                video.play()
+            }
+        
+        
+        
+//            // Create an AVPlayer, passing it the HTTP Live Streaming URL.
+//        let player = AVPlayer(url: url)
 //
-//            let controller = AVPlayerViewController()
-//            controller.player = video
+//            // Create a new AVPlayerViewController and pass it a reference to the player.
+//        let controller = AVPlayerViewController()
+//        controller.player = player
 //
-//                // Modally present the player and call the player's play() method when complete.
-//            present(controller, animated: true) {
-//                video.play()
-//            }
-        
-        
-        
-            // Create an AVPlayer, passing it the HTTP Live Streaming URL.
-        let player = AVPlayer(url: url)
-
-            // Create a new AVPlayerViewController and pass it a reference to the player.
-        let controller = AVPlayerViewController()
-        controller.player = player
-
-            // Modally present the player and call the player's play() method when complete.
-        present(controller, animated: true) {
-            player.volume = 100.0
-           // player.prepareToPlay()
-            player.play()
+//            // Modally present the player and call the player's play() method when complete.
+//        present(controller, animated: true) {
+//            player.volume = 100.0
+//           // player.prepareToPlay()
+//            player.play()
         }
     }
 }
